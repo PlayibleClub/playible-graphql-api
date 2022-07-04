6531b168e4bd5a2773467c4e04eab7a252ef4f88
Vince Gonzales <vincegonzalesdlsu@gmail.com>
2022-07-04 20:46:40 +0800

    updated content type s3 upload

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -263,7 +263,7 @@ export class TasksService {
               }
 
               result = convert.js2xml(result, options)
-              result = '<?xml version="1.0" encoding="utf-8"?>\n' + result
+              // result = '<?xml version="1.0" encoding="utf-8"?>\n' + result
               var buffer = Buffer.from(result, "utf8")
               const s3 = new S3({
                 accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -276,6 +276,7 @@ export class TasksService {
                 Bucket: process.env.AWS_BUCKET_NAME,
                 Key: `${s3_location}${filename}`,
                 Body: fileContent,
+                ContentType: "image/svg+xml",
               }
 
               s3.upload(params, async (err: any, data: any) => {
