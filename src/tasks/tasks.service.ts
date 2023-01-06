4537426d03803cc131b0ed1c20a676a7f036f879
Vince Gonzales <vincegonzalesdlsu@gmail.com>
2023-01-06 11:41:27 +0800

    Remove other nba assets

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -653,7 +653,7 @@ export class TasksService {
     this.logger.debug(`TOTAL ATHLETES: ${athletes.length}`)
   }
 
-  @Timeout(1)
+  // @Timeout(1)
   async generateAthleteNbaAssetsPromo() {
     this.logger.debug("Generate Athlete NBA Assets Promo: STARTED")
 
@@ -856,7 +856,7 @@ export class TasksService {
     this.logger.debug(`TOTAL ATHLETES: ${athletes.length}`)
   }
 
-  @Timeout(1)
+  // @Timeout(1)
   async generateAthleteNbaAssetsLocked() {
     this.logger.debug("Generate Athlete NBA Assets Locked: STARTED")
 
