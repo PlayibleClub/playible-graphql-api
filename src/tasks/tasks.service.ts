d9c381d9f8825960ef152f2f7b3d37f19e775eef
Vince Gonzales <vincegonzalesdlsu@gmail.com>
2023-01-10 00:26:46 +0800

    Bring back stats

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1397,7 +1397,7 @@ export class TasksService {
     }
   }
 
-  @Timeout(1)
+  // @Timeout(1)
   @Interval(300000) // Runs every 5 mins
   async updateNbaAthleteStatsPerDay() {
     this.logger.debug("Update NBA Athlete Stats Per Day: STARTED")
@@ -1411,7 +1411,7 @@ export class TasksService {
 
       if (timeFrame) {
         const season = timeFrame.ApiSeason
-        const date = moment().subtract(4, "day").toDate()
+        const date = moment().subtract(1, "day").toDate()
         const dateFormat = moment(date).format("YYYY-MMM-DD").toUpperCase()
 
         this.logger.debug(dateFormat)
