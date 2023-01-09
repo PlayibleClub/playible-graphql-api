f58be95a7c879938661aaa3b71f9d0f5f1cd802b
Vince Gonzales <vincegonzalesdlsu@gmail.com>
2023-01-10 00:08:47 +0800

    Disable generate image

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -857,7 +857,7 @@ export class TasksService {
     this.logger.debug(`TOTAL ATHLETES: ${athletes.length}`)
   }
 
-  @Timeout(1)
+  // @Timeout(1)
   async generateAthleteNbaAssetsLocked() {
     this.logger.debug("Generate Athlete NBA Assets Locked: STARTED")
 
