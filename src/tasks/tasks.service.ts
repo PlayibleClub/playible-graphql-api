2880a61075854dcf5e5d45aac3953be491dffc39
Vince Gonzales <vincegonzalesdlsu@gmail.com>
2023-01-10 00:05:57 +0800

    Generate nba locked

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -654,7 +654,7 @@ export class TasksService {
     this.logger.debug(`TOTAL ATHLETES: ${athletes.length}`)
   }
 
-  @Timeout(1)
+  // @Timeout(1)
   async generateAthleteNbaAssetsPromo() {
     this.logger.debug("Generate Athlete NBA Assets Promo: STARTED")
 
@@ -857,7 +857,7 @@ export class TasksService {
     this.logger.debug(`TOTAL ATHLETES: ${athletes.length}`)
   }
 
-  // @Timeout(1)
+  @Timeout(1)
   async generateAthleteNbaAssetsLocked() {
     this.logger.debug("Generate Athlete NBA Assets Locked: STARTED")
 
