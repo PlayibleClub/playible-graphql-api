ed1ab954255245d80f817249867c4db1cab6af28
Vince Gonzales <vincegonzalesdlsu@gmail.com>
2023-01-10 14:36:51 +0800

    Remove addAdmin

--- src/resolvers/User.ts
+++ src/resolvers/User.ts
@@ -1,33 +1,32 @@
-import argon from "argon2";
+import argon from "argon2"
 
-import { Arg, Field, Mutation, ObjectType, Resolver } from "type-graphql";
-import { AdminWallet } from "../entities/AdminWallet";
-import { User } from "../entities/User";
+import { Arg, Field, Mutation, ObjectType, Resolver } from "type-graphql"
+import { AdminWallet } from "../entities/AdminWallet"
+import { User } from "../entities/User"
 
 @ObjectType()
 class FieldError {
   @Field()
-  field?: string;
+  field?: string
   @Field()
-  message?: string;
+  message?: string
 }
 
 @ObjectType()
 class UserResponse {
   @Field(() => [FieldError], { nullable: true })
-  errors?: FieldError[];
+  errors?: FieldError[]
   @Field(() => User, { nullable: true })
-  user?: User;
+  user?: User
 }
 
 @Resolver()
 export class UserResolver {
-  @Mutation(() => String)
-  async addAdmin(@Arg("address") address: string): Promise<String> {
-    await AdminWallet.create({
-      address: await argon.hash(address),
-    }).save();
-
-    return "success";
-  }
+  // @Mutation(() => String)
+  // async addAdmin(@Arg("address") address: string): Promise<String> {
+  //   await AdminWallet.create({
+  //     address: await argon.hash(address),
+  //   }).save();
+  //   return "success";
+  // }
 }
