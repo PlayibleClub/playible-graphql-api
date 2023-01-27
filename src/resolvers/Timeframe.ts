d996b191f702ef4510420c7ed3d66e48198c2df1
Ramon Arca <ramonteodoroarca@gmail.com>
2023-01-27 10:12:32 +0800

    Add resolver for getting the current nba season

--- src/resolvers/Timeframe.ts
+++ src/resolvers/Timeframe.ts
@@ -1,6 +1,7 @@
 import { Arg, Authorized, Field, Mutation, ObjectType, Query, Resolver } from 'type-graphql'
 import { Timeframe } from '../entities/Timeframe'
 import { MoreThan, LessThan, Between } from "typeorm" 
+import { SportType } from '../utils/types'
 @Resolver()
 export class TimeframeResolver {
 
@@ -16,5 +17,13 @@ export class TimeframeResolver {
       }
     })
     
+  }
+  @Query(() => Timeframe)
+  async getNbaCurrentSeason(): Promise<Timeframe>{
+    return await Timeframe.findOneOrFail({
+      where: {
+        sport: SportType.NBA
+      }
+    })
   }
 }
