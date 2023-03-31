1083fec23f9c6442e6e184b10db7581e96f29268
Ramon Arca <ramonteodoroarca@gmail.com>
2023-03-31 20:43:34 +0800

    Add addStarterCricketAthletes fn to Cricket resolver

--- src/resolvers/CricketAthlete.ts
+++ src/resolvers/CricketAthlete.ts
@@ -1,10 +1,44 @@
 import { AthleteStatType, SportType } from './../utils/types'
 import { Arg, Authorized, Field, Mutation, ObjectType, Query, Resolver } from 'type-graphql'
-import { IsNull, Not} from 'typeorm'
+import { IsNull, Not, In} from 'typeorm'
 import { CricketAthlete } from '../entities/CricketAthlete'
 import { CricketAthleteStat } from '../entities/CricketAthleteStat'
 import { CricketTeam } from '../entities/CricketTeam'
+import { Contract } from "near-api-js"
 import moment from 'moment'
+import { setup } from "../near-api"
+//import CRICKET_ATHLETE_IDS, CRICKET_ATHLETE_PROMO_IDS
+
+const chunkify = (a: any[], n: number, balanced: boolean) => {
+  if (n < 2) return [a]
+
+  var len = a.length,
+    out = [],
+    i = 0,
+    size
+
+  if (len % n === 0) {
+    size = Math.floor(len / n)
+    while (i < len) {
+      out.push(a.slice(i, (i += size)))
+    }
+  } else if (balanced) {
+    while (i < len) {
+      size = Math.ceil((len - i) / n--)
+      out.push(a.slice(i, (i += size)))
+    }
+  } else {
+    n--
+    size = Math.floor(len / n)
+    if (len % size === 0) size--
+    while (i < size * n) {
+      out.push(a.slice(i, (i += size)))
+    }
+    out.push(a.slice(size * n))
+  }
+
+  return out
+}
 @Resolver()
 export class CricketAthleteResolver {
   @Query(() => CricketAthlete)
@@ -96,5 +130,63 @@ export class CricketAthleteResolver {
   // async getAthlete(
 
   // )
-  
+  @Authorized("ADMIN")
+  @Mutation(() => Number)
+  async addStarterCricketAthletesToOpenPackContract(@Arg("isPromo") isPromo: boolean = false): Promise<Number>{
+    let contractId
+    let athleteIds: number[] = []
+
+    switch(isPromo){
+      case false:
+        contractId = process.env.OPENPACK_CRICKET_ACCOUNT_ID
+        //add athlete id list here
+        break
+      case true:
+        contractId = process.env.OPENPACK_CRICKET_PROMO_ACCOUNT_ID
+        //add promo athlete id list here
+        break
+      
+    }
+
+    const nearApi = await setup()
+    const account = await nearApi.account(process.env.NEAR_MAIN_ACCOUNT_ID || "")
+    const contract: any = new Contract(account, contractId || "", {
+      viewMethods: [],
+      changeMethods: ["execute_add_athletes"],
+    })
+
+    const athleteTokens = (await CricketAthlete.find({ where: {playerKey: In(['a', 'b'])}, order: { id: "ASC"}, relations: { cricketTeam: true}})).map(
+      (athlete) => {
+        if (isPromo){
+          return {
+            athlete_id: athlete.id.toString(),
+            soulbound_token_uri: athlete.nftImageLocked,
+            single_use_token_uri: athlete.nftImagePromo,
+            symbol: athlete.playerKey,
+            name: athlete.name,
+            team: athlete.cricketTeam.key,
+            position: athlete.seasonalRole,
+          }
+        } else{
+          return {
+            athlete_id: athlete.id.toString(),
+            token_uri: athlete.nftImage,
+            symbol: athlete.playerKey,
+            name: athlete.name,
+            team: athlete.cricketTeam.key,
+            position: athlete.seasonalRole
+          }
+        }
+      }
+    )
+
+    const chunkifiedAthleteTokens = chunkify(athleteTokens, 10, false)
+
+    for (const _athleteTokens of chunkifiedAthleteTokens) {
+      await contract.execute_add_athletes({ pack_type: "starter", athlete_tokens: _athleteTokens}, "300000000000000")
+
+    }
+
+    return athleteTokens.length
+  }
 }
