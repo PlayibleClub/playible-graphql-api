aad022edfd14d284e1b6f2ee9caf3c958688e44c
Ramon Arca <ramonteodoroarca@gmail.com>
2023-04-24 13:30:35 +0800

    Add cricket athlete id list

--- src/utils/athlete-ids/index.ts
+++ src/utils/athlete-ids/index.ts
@@ -3,3 +3,4 @@ export { NBA_ATHLETE_IDS } from "./nba"
 export { NBA_ATHLETE_PROMO_IDS } from "./nba-promo"
 export { MLB_ATHLETE_IDS} from './mlb'
 export { MLB_ATHLETE_PROMO_IDS } from './mlb-promo'
+export { IPL2023_ATHLETE_IDS } from './ipl2023'
