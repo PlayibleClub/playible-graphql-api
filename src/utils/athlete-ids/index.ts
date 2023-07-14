1a6072371837ab78b76f8db59d8be4b015231660
Ramon Arca <ramonteodoroarca@gmail.com>
2023-07-14 17:14:20 +0800

    Add new athlete IDs for MLB

--- src/utils/athlete-ids/index.ts
+++ src/utils/athlete-ids/index.ts
@@ -1,6 +1,6 @@
 export { NFL_ATHLETE_IDS } from "./nfl"
 export { NBA_ATHLETE_IDS } from "./nba"
 export { NBA_ATHLETE_PROMO_IDS } from "./nba-promo"
-export { MLB_ATHLETE_IDS} from './mlb'
-export { MLB_ATHLETE_PROMO_IDS } from './mlb-promo'
+export { MLB_ATHLETE_IDS} from './mlb-new'
+export { MLB_ATHLETE_PROMO_IDS } from './mlb-promo-new'
 export { IPL2023_ATHLETE_IDS } from './ipl2023'
