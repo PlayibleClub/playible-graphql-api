69c860767bd95dfe978aaed7fea0a6f4b0f19774
Ramon Arca <ramonteodoroarca@gmail.com>
2023-08-29 09:30:01 +0800

    Add new NFL and NFL_PROMO athlete ids

--- src/utils/athlete-ids/index.ts
+++ src/utils/athlete-ids/index.ts
@@ -1,4 +1,5 @@
-export { NFL_ATHLETE_IDS } from "./nfl"
+export { NFL_ATHLETE_IDS } from "./nfl-new"
+export { NFL_ATHLETE_PROMO_IDS } from './nfl-promo-new'
 export { NBA_ATHLETE_IDS } from "./nba"
 export { NBA_ATHLETE_PROMO_IDS } from "./nba-promo"
 export { MLB_ATHLETE_IDS} from './mlb-new'
