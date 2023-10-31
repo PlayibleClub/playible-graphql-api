096b51da81b25d61d1a0bd95bef81e1c6449d0b2
Ramon Arca <ramonteodoroarca@gmail.com>
2023-11-01 01:49:49 +0800

    ChangeAthleteMetadata setup

--- src/near-api.ts
+++ src/near-api.ts
@@ -1,14 +1,14 @@
-import * as nearAPI from "near-api-js";
+import * as nearAPI from 'near-api-js';
 const { keyStores, KeyPair, connect } = nearAPI;
-
+import { SportType } from './utils/types';
 export const setup = async () => {
   const keyStore = new keyStores.InMemoryKeyStore();
-  const keyPair = KeyPair.fromString(process.env.NEAR_PRIVATE_KEY_WALLET || "");
+  const keyPair = KeyPair.fromString(process.env.NEAR_PRIVATE_KEY_WALLET || '');
   await keyStore.setKey(
-    process.env.NEAR_NETWORK_ID ? process.env.NEAR_NETWORK_ID : "testnet",
+    process.env.NEAR_NETWORK_ID ? process.env.NEAR_NETWORK_ID : 'testnet',
     process.env.NEAR_MAIN_ACCOUNT_ID
       ? process.env.NEAR_MAIN_ACCOUNT_ID
-      : "playible.testnet",
+      : 'playible.testnet',
     keyPair
   );
 
@@ -21,6 +21,50 @@ export const setup = async () => {
     explorerUrl: `https://explorer.${process.env.NEAR_NETWORK_ID}.near.org`,
   };
 
+  const near = await connect(config);
+  return near;
+};
+
+export const changeAthleteMetadataSetup = async (
+  sport: SportType
+  //isPromo: boolean
+) => {
+  const keyStore = new keyStores.InMemoryKeyStore();
+  let keyString: string = '';
+  let accountId = '';
+  switch (sport) {
+    case SportType.NFL:
+      break;
+    case SportType.NFL_PROMO:
+      break;
+    case SportType.NBA:
+      keyString = process.env.NEAR_BASKETBALL_ATHLETE_KEY || ''; // change to specific sport var
+      accountId = process.env.NEAR_BASKETBALL_ATHLETE_ACCOUNT_ID || '';
+
+      break;
+    case SportType.NBA_PROMO:
+      break;
+    case SportType.MLB:
+      break;
+    case SportType.MLB_PROMO:
+      break;
+    default:
+      break;
+  }
+  const keyPair = KeyPair.fromString(keyString);
+  await keyStore.setKey(
+    process.env.NEAR_NETWORK_ID ? process.env.NEAR_NETWORK_ID : 'testnet',
+    accountId,
+    keyPair
+  );
+  const config: any = {
+    networkId: process.env.NEAR_NETWORK_ID,
+    keyStore,
+    nodeUrl: `https://rpc.${process.env.NEAR_NETWORK_ID}.near.org`,
+    walletUrl: `https://wallet.${process.env.NEAR_NETWORK_ID}.near.org`,
+    helperUrl: `https://helper.${process.env.NEAR_NETWORK_ID}.near.org`,
+    explorerUrl: `https://explorer.${process.env.NEAR_NETWORK_ID}.near.org`,
+  };
   const near = await connect(config);
   return near;
 };
