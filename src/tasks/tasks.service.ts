21cdd02b205f7647796ffe0dda10f8bae0ebc904
Ramon Arca <ramonteodoroarca@gmail.com>
2023-11-21 15:21:40 +0800

    Enable Lake Framework on dev

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -4744,7 +4744,7 @@ export class TasksService {
     }
   }
 
-  //@Timeout(1)
+  @Timeout(1)
   async runNearLakeFrameworkIndexer() {
     const lakeConfig: types.LakeConfig = {
       //credentials
