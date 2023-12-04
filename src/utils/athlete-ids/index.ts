5e1d31917257c976775648f91d1d947feb97ea15
Ramon Arca <ramonteodoroarca@gmail.com>
2023-12-04 20:42:20 +0800

    Add nfl-missing athleteIds

--- src/utils/athlete-ids/index.ts
+++ src/utils/athlete-ids/index.ts
@@ -1,4 +1,4 @@
-export { NFL_ATHLETE_IDS } from './nfl-new';
+export { NFL_ATHLETE_IDS } from './nfl-missing';
 export { NFL_ATHLETE_PROMO_IDS } from './nfl-promo-new';
 export { NBA_ATHLETE_IDS } from './nba-new';
 export { NBA_ATHLETE_PROMO_IDS } from './nba-promo-new';
