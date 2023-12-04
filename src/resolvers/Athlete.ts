b6c4a7e7b33e5f05218a7cc5ef5ecc3de52d65fc
Ramon Arca <ramonteodoroarca@gmail.com>
2023-12-04 19:37:55 +0800

    Add setTimeout for getAthleteApiIFromContract

--- src/resolvers/Athlete.ts
+++ src/resolvers/Athlete.ts
@@ -506,6 +506,7 @@ export class AthleteResolver {
       });
       console.log(result);
       apiIdFromContract.push(result[2]);
+      await new Promise((resolve) => setTimeout(resolve, 2000));
     }
     return apiIdFromContract;
   }
